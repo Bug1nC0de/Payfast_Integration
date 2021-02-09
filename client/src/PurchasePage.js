2d3b2e9d25e62f56f7ec0d5f11ad7f47743d9f07
007 <katlego@callitservices.co.za>
2021-02-09 08:29:45 +0200

    deploy

--- client/src/PurchasePage.js
+++ client/src/PurchasePage.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { Row, Form, Button, Card, InputGroup } from "react-bootstrap";
-import axios from 'axios'
-import crypto from 'crypto'
+import axios from "axios";
+import crypto from "crypto";
 
 const PurchasPage = () => {
 	const [quantity, setQuantity] = useState(0);
 	const [total, setTotal] = useState(0);
-
 
 	const calculateTotal = () => {
 		if (quantity <= 0) {
@@ -24,47 +23,47 @@ const PurchasPage = () => {
 	//HANDLE PAYMENT CANCELLED
 	//HANDLE PAYMENT FAILURE
 	//Must be HERE//
-	const paymentSuccess = () =>  console.log("PAYMENT SUCCESS")
-	const paymentCancelled = () => console.log("PAYMENT CANCELLED")
-
-
-
+	const paymentSuccess = () => console.log("PAYMENT SUCCESS");
+	const paymentCancelled = () => console.log("PAYMENT CANCELLED");
 
 	const initiatePayment = async () => {
 		const myData = {
-			"merchant_id": "10000100",
-			"merchant_key": "46f0cd694581a",
+			merchant_id: "10000100",
+			merchant_key: "46f0cd694581a",
 			//where your app goes after successfull payment
-			"return_url":"www.google.com",
+			return_url: "www.google.com",
 
 			//where app goes after payment fail or cancel
-			"cancel_url":"www.google.com",
+			cancel_url: "www.google.com",
 
 			//backend url where www.yourbackend.com/fastpay-payment-success
-			"notify_url":"www.google.com",
+			notify_url: "www.google.com",
 
 			//customer details
-			"name_first":"Customer Name",
-			"name_last":"Customer Last name",
-			"email_address":"sample_mail@gmail.com",
+			name_first: "Customer Name",
+			name_last: "Customer Last name",
+			email_address: "sample_mail@gmail.com",
 
 			//transaction details
-			"amount":total.toString(),
-			"item_name":"test Item name",
+			amount: total.toString(),
+			item_name: "test Item name",
 
 			//for email confirmation
-			"email_confirmation":"1",
-			"confirmation_address":"sample_mail@gmail.com"
+			email_confirmation: "1",
+			confirmation_address: "sample_mail@gmail.com",
 		};
-		const passPhrase = '';
+		const passPhrase = "";
 		const generateSignature = (data, passPhrase = null) => {
 			// Create parameter string
 			let pfOutput = "";
 			for (let key in data) {
-				if(data.hasOwnProperty(key)){
+				if (data.hasOwnProperty(key)) {
 					if (data[key] !== "") {
-						console.log(data[key])
-						pfOutput +=`${key}=${encodeURIComponent(data[key].trim()).replace(/%20/g, " + ")}&`
+						console.log(data[key]);
+						pfOutput += `${key}=${encodeURIComponent(data[key].trim()).replace(
+							/%20/g,
+							" + "
+						)}&`;
 					}
 				}
 			}
@@ -72,7 +71,9 @@ const PurchasPage = () => {
 			// Remove last ampersand
 			let getString = pfOutput.slice(0, -1);
 			if (passPhrase !== null) {
-				getString +=`&passphrase=${encodeURIComponent(passPhrase.trim()).replace(/%20/g, "+")}`;
+				getString += `&passphrase=${encodeURIComponent(
+					passPhrase.trim()
+				).replace(/%20/g, "+")}`;
 			}
 
 			return crypto.createHash("md5").update(getString).digest("hex");
@@ -81,44 +82,48 @@ const PurchasPage = () => {
 			// Convert your data array to a string
 			let pfParamString = "";
 			for (let key in dataArray) {
-				if(dataArray.hasOwnProperty(key)){pfParamString +=`${key}=${encodeURIComponent(dataArray[key].trim()).replace(/%20/g, "+")}&`;}
+				if (dataArray.hasOwnProperty(key)) {
+					pfParamString += `${key}=${encodeURIComponent(
+						dataArray[key].trim()
+					).replace(/%20/g, "+")}&`;
+				}
 			}
 			// Remove last ampersand
 			return pfParamString.slice(0, -1);
 		};
 
 		const generatePaymentIdentifier = async (pfParamString) => {
-			const result = await axios.post(`https://www.payfast.co.za/onsite/process`, pfParamString)
+			const result = await axios
+				.post(`https://www.payfast.co.za/onsite/process`, pfParamString)
 				.then((res) => {
 					return res.data.uuid || null;
 				})
 				.catch((error) => {
-					console.error(error)
+					console.error(error);
 				});
 			console.log("res.data", result);
 			return result;
 		};
 
-// Generate signature (see Custom Integration -> Step 2)
+		// Generate signature (see Custom Integration -> Step 2)
 		myData["signature"] = generateSignature(myData, passPhrase);
 
-// Convert the data array to a string
+		// Convert the data array to a string
 		const pfParamString = dataToString(myData);
 
-// Generate payment identifier
+		// Generate payment identifier
 		const identifier = await generatePaymentIdentifier(pfParamString);
 
-		window.payfast_do_onsite_payment({"uuid": identifier}, function (result) {
+		window.payfast_do_onsite_payment({ uuid: identifier }, function (result) {
 			if (result === true) {
 				// Payment Completed
-				paymentSuccess()
-			}
-			else {
+				paymentSuccess();
+			} else {
 				// Payment Window Closed
-				paymentCancelled()
+				paymentCancelled();
 			}
 		});
-	}
+	};
 
 	return (
 		<div>
@@ -128,40 +133,43 @@ const PurchasPage = () => {
 					{/*	action="https://payfast.co.za/eng/process" method="post"*/}
 					{/*>*/}
 
+					<Card.Header className="bg-dark text-white mb-2">
+						Purchase ITEM
+					</Card.Header>
+					<Form.Group>
+						How many ITEMS would you like to purchase?
+						<InputGroup>
+							<Form.Control
+								required
+								type="number"
+								min="0"
+								value={quantity}
+								onChange={(e) => setQuantity(e.target.value)}
+							/>
+							<InputGroup.Append>
+								<Button onClick={calculateTotal}>
+									Confirm <i className="far fa-check-circle"></i>
+								</Button>
+							</InputGroup.Append>
+						</InputGroup>
+					</Form.Group>
 
-
-						<Card.Header className="bg-dark text-white mb-2">
-							Purchase ITEM
-						</Card.Header>
-						<Form.Group>
-							How many ITEMS would you like to purchase?
-							<InputGroup>
-								<Form.Control
-									required
-									type="number"
-									min="0"
-									value={quantity}
-									onChange={(e) => setQuantity(e.target.value)}
-								/>
-								<InputGroup.Append>
-									<Button onClick={calculateTotal}>
-										Confirm <i className="far fa-check-circle"></i>
-									</Button>
-								</InputGroup.Append>
-							</InputGroup>
-						</Form.Group>
-
-						{total <= 0 ? (
-							<Button variant="outline-info text-success" block disabled>
-								Total R{total}, Confirm number of tunnels to pay{" "}
-								<i className="fas fa-credit-card ml-2"></i>
-							</Button>
-						) : (
-							<Button variant="success" block type="submit" onClick={initiatePayment}>
-								Your total is R{total} Click to Pay{" "}
-								<i className="fas fa-credit-card ml-2"></i>
-							</Button>
-						)}
+					{total <= 0 ? (
+						<Button variant="outline-info text-success" block disabled>
+							Total R{total}, Confirm number of tunnels to pay{" "}
+							<i className="fas fa-credit-card ml-2"></i>
+						</Button>
+					) : (
+						<Button
+							variant="success"
+							block
+							type="submit"
+							onClick={initiatePayment}
+						>
+							Your total is R{total} Click to Pay{" "}
+							<i className="fas fa-credit-card ml-2"></i>
+						</Button>
+					)}
 					{/*</Form>*/}
 				</Card>
 			</Row>
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const connectDB = require('./utils/db');
-
+const express = require("express");
+const connectDB = require("./utils/db");
 
 const app = express();
 connectDB();
+
+app.post("/fastpay-payment-success", (req, res, next) => {
+	// Payment Related Meta Data
+	// Payment Success Webhook
+	console.log(req.body);
+	res.status(200);
+});
+
+//Check if we in production//
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
-app.post("/fastpay-payment-success",(req,res,next)=>{
-    // Payment Related Meta Data
-    // Payment Success Webhook
-    console.log(req.body)
-    res.status(200)
-})
+
 app.listen(PORT, () => console.log(`Template App running on port ${PORT}`));
