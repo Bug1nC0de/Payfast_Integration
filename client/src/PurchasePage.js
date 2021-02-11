09ece8257f9a47394a1d690d5f9bf2591c705809
007 <katlego@callitservices.co.za>
2021-02-11 18:56:04 +0200

    Deploy for testing

--- client/src/PurchasePage.js
+++ client/src/PurchasePage.js
@@ -1,97 +1,98 @@
-import React, {useState} from "react";
-import {Row, Form, Button, Card, InputGroup} from "react-bootstrap";
-import axios from 'axios'
-import crypto from 'crypto'
+import React, { useState } from "react";
+import { Row, Form, Button, Card, InputGroup } from "react-bootstrap";
+import axios from "axios";
 
 const PurchasPage = () => {
-    const [quantity, setQuantity] = useState(0);
-    const [total, setTotal] = useState(0);
-    const [isLoading, setLoading] = useState(false)
+	const [quantity, setQuantity] = useState(0);
+	const [total, setTotal] = useState(0);
+	const [isLoading, setLoading] = useState(false);
 
+	const calculateTotal = () => {
+		if (quantity <= 0) {
+			setTotal(0);
+			console.log("Stop playing Games");
+		} else {
+			let amount = quantity * 20;
+			setTotal(amount);
+		}
+	};
 
-    const calculateTotal = () => {
-        if (quantity <= 0) {
-            setTotal(0);
-            console.log("Stop playing Games");
-        } else {
-            let amount = quantity * 20;
-            setTotal(amount);
-        }
-    };
+	//Payfast API Function//
+	//The function to post to the payfast API
+	//HANDLE THE PAYMENT SUCCESSFULL
+	//HANDLE PAYMENT CANCELLED
+	//HANDLE PAYMENT FAILURE
+	//Must be HERE//
+	const paymentSuccess = () => console.log("PAYMENT SUCCESS");
+	const paymentCancelled = () => console.log("PAYMENT CANCELLED");
 
-    //Payfast API Function//
-    //The function to post to the payfast API
-    //HANDLE THE PAYMENT SUCCESSFULL
-    //HANDLE PAYMENT CANCELLED
-    //HANDLE PAYMENT FAILURE
-    //Must be HERE//
-    const paymentSuccess = () => console.log("PAYMENT SUCCESS")
-    const paymentCancelled = () => console.log("PAYMENT CANCELLED")
+	const initiatePayment = async () => {
+		setLoading(true);
+		const response = await axios.post(
+			"http://localhost:5000/get-payment-token",
+			{ email_address: "sample@gmail.com", amount: total, item_name: "test" }
+		);
+		window.payfast_do_onsite_payment(response.data, function (result) {
+			if (result === true) {
+				// Payment Completed
+				setLoading(false);
+				paymentSuccess();
+			} else {
+				// Payment Window Closed
+				setLoading(false);
+				paymentCancelled();
+			}
+		});
+	};
 
+	return (
+		<div>
+			<Row className="justify-content-center">
+				<Card style={{ border: "0px", width: "25rem" }}>
+					<Card.Header className="bg-dark text-white mb-2">
+						Purchase ITEM
+					</Card.Header>
+					<Form.Group>
+						How many ITEMS would you like to purchase?
+						<InputGroup>
+							<Form.Control
+								required
+								type="number"
+								min="0"
+								value={quantity}
+								onChange={(e) => setQuantity(e.target.value)}
+							/>
+							<InputGroup.Append>
+								<Button onClick={calculateTotal}>
+									Confirm <i className="far fa-check-circle"></i>
+								</Button>
+							</InputGroup.Append>
+						</InputGroup>
+					</Form.Group>
 
-    const initiatePayment = async () => {
-        setLoading(true)
-        const response = await axios.post("http://localhost:5000/get-payment-token",{email_address:"sample@gmail.com",amount:total,item_name:"test"})
-        window.payfast_do_onsite_payment(response.data, function (result) {
-            if (result === true) {
-                // Payment Completed
-                setLoading(false)
-                paymentSuccess()
-            } else {
-                // Payment Window Closed
-                setLoading(false)
-                paymentCancelled()
-            }
-        });
-    }
-
-    return (
-        <div>
-            <Row className="justify-content-center">
-                <Card style={{border: "0px", width: "25rem"}}>
-                    {/*<Form*/}
-                    {/*	action="https://payfast.co.za/eng/process" method="post"*/}
-                    {/*>*/}
-
-
-                    <Card.Header className="bg-dark text-white mb-2">
-                        Purchase ITEM
-                    </Card.Header>
-                    <Form.Group>
-                        How many ITEMS would you like to purchase?
-                        <InputGroup>
-                            <Form.Control
-                                required
-                                type="number"
-                                min="0"
-                                value={quantity}
-                                onChange={(e) => setQuantity(e.target.value)}
-                            />
-                            <InputGroup.Append>
-                                <Button onClick={calculateTotal}>
-                                    Confirm <i className="far fa-check-circle"></i>
-                                </Button>
-                            </InputGroup.Append>
-                        </InputGroup>
-                    </Form.Group>
-
-                    {total <= 0 ? (
-                        <Button variant="outline-info text-success" block disabled>
-                            Total R{total}, Confirm number of tunnels to pay{" "}
-                            <i className="fas fa-credit-card ml-2"></i>
-                        </Button>
-                    ) : (
-                        <Button disabled={isLoading} variant="success" block type="submit" onClick={initiatePayment}>
-                            {isLoading?"Getting Meta Data for Payment...":`Your total is R${total} Click to Pay`}
-                            <i className="fas fa-credit-card ml-2"></i>
-                        </Button>
-                    )}
-
-                    {/*</Form>*/}
-                </Card>
-            </Row>
-        </div>
-    );
+					{total <= 0 ? (
+						<Button variant="outline-info text-success" block disabled>
+							Total R{total}, Confirm number of tunnels to pay{" "}
+							<i className="fas fa-credit-card ml-2"></i>
+						</Button>
+					) : (
+						<Button
+							disabled={isLoading}
+							variant="success"
+							block
+							type="submit"
+							onClick={initiatePayment}
+						>
+							{isLoading
+								? "Getting Meta Data for Payment..."
+								: `Your total is R${total} Click to Pay`}
+							<i className="fas fa-credit-card ml-2"></i>
+						</Button>
+					)}
+				</Card>
+			</Row>
+		</div>
+	);
 };
 
 export default PurchasPage;
--- index.js
+++ index.js
@@ -1,90 +1,95 @@
-const express = require('express');
-const axios = require('axios')
-const crypto = require('crypto')
-const cors = require('cors')
-// const connectDB = require('./utils/db');
-
+const express = require("express");
+const axios = require("axios");
+const crypto = require("crypto");
+const cors = require("cors");
+const connectDB = require("./utils/db");
 
 const app = express();
-app.use(cors())
-app.use(express.json())
-// connectDB();
-const PORT = process.env.PORT || 5000;
-app.get("/status-check",(req,res)=> res.send("OK") )
+app.use(cors());
+app.use(express.json());
+connectDB();
 
-app.post("/get-payment-token",async (req,res,next)=>{
-    console.log(req.body)
-    const myData = {
-        "merchant_id": "10798473",
-        "merchant_key": "qtbv3djb4afpj",
-        "email_address": req.body.email_address,
-        "amount": parseFloat(req.body.amount).toString(),
-        "item_name": req.body.item_name,
+app.get("/status-check", (req, res) => res.send("OK"));
 
-    };
-    const generateSignature = (data, passPhrase = null) => {
-        // Create parameter string
-        let pfOutput = "";
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                if (data[key] !== "") {
-                    console.log(data[key])
-                    pfOutput += `${key}=${encodeURIComponent(data[key].trim()).replace(/%20/g, " + ")}&`
-                }
-            }
-        }
+app.post("/get-payment-token", async (req, res, next) => {
+	console.log(req.body);
+	const myData = {
+		merchant_id: "10798473",
+		merchant_key: "qtbv3djb4afpj",
+		email_address: req.body.email_address,
+		amount: parseFloat(req.body.amount).toString(),
+		item_name: req.body.item_name,
+	};
+	const generateSignature = (data, passPhrase = null) => {
+		// Create parameter string
+		let pfOutput = "";
+		for (let key in data) {
+			if (data.hasOwnProperty(key)) {
+				if (data[key] !== "") {
+					console.log(data[key]);
+					pfOutput += `${key}=${encodeURIComponent(data[key].trim()).replace(
+						/%20/g,
+						" + "
+					)}&`;
+				}
+			}
+		}
 
-        // Remove last ampersand
-        let getString = pfOutput.slice(0, -1);
-        if (passPhrase !== null) {
-            getString += `&passphrase=${encodeURIComponent(passPhrase.trim()).replace(/%20/g, "+")}`;
-        }
+		// Remove last ampersand
+		let getString = pfOutput.slice(0, -1);
+		if (passPhrase !== null) {
+			getString += `&passphrase=${encodeURIComponent(passPhrase.trim()).replace(
+				/%20/g,
+				"+"
+			)}`;
+		}
 
-        return crypto.createHash("md5").update(getString).digest("hex");
-    };
-    const dataToString = (dataArray) => {
-        // Convert your data array to a string
-        let pfParamString = "";
-        for (let key in dataArray) {
-            if (dataArray.hasOwnProperty(key)) {
-                pfParamString += `${key}=${encodeURIComponent(dataArray[key].trim()).replace(/%20/g, "+")}&`;
-            }
-        }
-        // Remove last ampersand
-        return pfParamString.slice(0, -1);
-    };
+		return crypto.createHash("md5").update(getString).digest("hex");
+	};
+	const dataToString = (dataArray) => {
+		// Convert your data array to a string
+		let pfParamString = "";
+		for (let key in dataArray) {
+			if (dataArray.hasOwnProperty(key)) {
+				pfParamString += `${key}=${encodeURIComponent(
+					dataArray[key].trim()
+				).replace(/%20/g, "+")}&`;
+			}
+		}
+		// Remove last ampersand
+		return pfParamString.slice(0, -1);
+	};
 
-    const generatePaymentIdentifier = async (pfParamString) => {
-        const result = await axios.post(`https://www.payfast.co.za/onsite/process`, pfParamString)
-            .then((res) => {
-                return res.data.uuid || null;
-            })
-            .catch((error) => {
-                console.error(error.message)
-            });
-        return result;
-    };
+	const generatePaymentIdentifier = async (pfParamString) => {
+		const result = await axios
+			.post(`https://www.payfast.co.za/onsite/process`, pfParamString)
+			.then((res) => {
+				return res.data.uuid || null;
+			})
+			.catch((error) => {
+				console.error(error.message);
+			});
+		return result;
+	};
 
-// Generate signature (see Custom Integration -> Step 2)
-    myData["signature"] = generateSignature(myData);
+	// Generate signature (see Custom Integration -> Step 2)
+	myData["signature"] = generateSignature(myData);
 
-// Convert the data array to a string
-    const pfParamString = dataToString(myData);
+	// Convert the data array to a string
+	const pfParamString = dataToString(myData);
 
-// Generate payment identifier
-    const identifier = await generatePaymentIdentifier(pfParamString);
+	// Generate payment identifier
+	const identifier = await generatePaymentIdentifier(pfParamString);
 
-    res.json({uuid:identifier})
+	res.json({ uuid: identifier });
+});
 
-
-})
-app.post("/fastpay-payment-success",(req,res,next)=>{
-    // Payment Related Meta Data
-    // Payment Success Webhook
-    console.log(req.body)
-    res.status(200)
-})
-
+app.post("/fastpay-payment-success", (req, res, next) => {
+	// Payment Related Meta Data
+	// Payment Success Webhook
+	console.log(req.body);
+	res.status(200);
+});
 
 //Check if we in production//
 if (process.env.NODE_ENV === "production") {
